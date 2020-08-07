82ac5a22da6ad6bd13dbfb20ca7714766bd73875
殇感美 <939257783@qq.com>
2020-08-07 20:54:32 +0800

    更新模型仓库

--- src/Repository.php
+++ src/Repository.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 namespace HyperfStore;
 
 use Closure;
-use RuntimeException;
 use Hyperf\DbConnection\Model\Model;
 use Hyperf\Utils\ApplicationContext;
+use Hyperf\Database\Exception\QueryException;
 
 class Repository
 {
@@ -14,13 +14,18 @@ class Repository
     protected $model;
     
     // 依赖注入
-    public static function make()
+    public static function make(Model $model = null)
     {
-        return ApplicationContext::getContainer()->make(static::class);
+        $repository = ApplicationContext::getContainer()->make(static::class);
+        
+        // 放入模型
+        $repository->model($model ?? $repository->build());
+        
+        return $repository;
     }
     
     // 放入或使用模型
-    public function model($model = null)
+    public function model(Model $model = null)
     {
         if($model)
         {
@@ -41,5 +46,32 @@ class Repository
             
             $function();
         }
+    }
+
+    // 获取数据
+    public function __get($name)
+    {
+        return $this->model->$name;
+    }
+    
+    // 捕捉错误
+    protected function exception(Closure $function)
+    {
+        $function = $function->bindTo($this);
+        
+        try
+        {
+            return $function();
+        }
+        catch(QueryException $e)
+        {
+            return false;
+        }
+    }
+    
+    // 保存
+    protected function save($data)
+    {
+        return $this->model->fill($data)->save();
     }
 }
